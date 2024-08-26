2755a2d14caf94ab0fc80307acfb032019359694
grbzemir <eyetkingurbuz@trakya.edu.tr>
2024-08-26 14:53:59 +0300

    DeleteMessage

--- ToDoTypeScript/src/App.tsx
+++ ToDoTypeScript/src/App.tsx
@@ -7,6 +7,8 @@ import Message from "./Components/Message";
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
   const [todos, setTodos] = useState<Todos[]>([]);
+  const [editingId, setEditingId] = useState<number | null>(null);
+  const [editingMessage, setEditingMessage] = useState<string>("");
   // [
   //   {
   //     id: 1,
@@ -35,14 +37,23 @@ const App: React.FC = () => {
   };
 
   const updateMessage = (id: number) => {
+    setEditingId(id);
+    const todoToEdit = todos.find((todo) => todo.id === id);
+    if (todoToEdit) {
+      setEditingMessage(todoToEdit.message);
+    }
+  };
+  const editMessage = () => {
     setTodos(
       todos.map((todo) => {
-        if (todo.id === id) {
-          todo.message = todo.message + " - Güncellendi";
+        if (todo.id === editingId) {
+          return { ...todo, message: editingMessage };
         }
         return todo;
       })
     );
+    setEditingId(null);
+    setEditingMessage("");
   };
 
   return (
@@ -52,6 +63,10 @@ const App: React.FC = () => {
         todos={todos}
         deleteMessage={deleteMessage}
         updateMessage={updateMessage}
+        editingId={editingId}
+        editingMessage={editingMessage}
+        setEditingMessage={setEditingMessage}
+        editMessage={editMessage}
       />
     </div>
   );
--- ToDoTypeScript/src/Components/Message.tsx
+++ ToDoTypeScript/src/Components/Message.tsx
@@ -5,20 +5,50 @@ type Props = {
   todos: Todos[];
   deleteMessage: (id: number) => void;
   updateMessage: (id: number) => void;
+  editingId: number | null;
+  editingMessage: string;
+  setEditingMessage: React.Dispatch<React.SetStateAction<string>>;
+  editMessage: () => void;
 };
 
-const Message: React.FC<Props> = ({ todos, deleteMessage, updateMessage }) => {
+const Message: React.FC<Props> = ({
+  todos,
+  deleteMessage,
+  updateMessage,
+  editingId,
+  editingMessage,
+  setEditingMessage,
+  editMessage,
+}) => {
   return (
     <div>
-      {todos.map((todo, i) => (
-        <div key={i}>
-          {todo.message} -{" "}
-          <span
-            onClick={() => deleteMessage(todo.id)}
-            style={{ cursor: "pointer" }}
-          >
-            x
-          </span>
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          {editingId === todo.id ? (
+            <div>
+              <input
+                value={editingMessage}
+                onChange={(e) => setEditingMessage(e.target.value)}
+              />
+              <button onClick={editMessage}>Kaydet</button>
+            </div>
+          ) : (
+            <div>
+              {todo.message}
+              <span
+                onClick={() => updateMessage(todo.id)}
+                style={{ cursor: "pointer", marginLeft: "10px" }}
+              >
+                Güncelle
+              </span>
+              <span
+                onClick={() => deleteMessage(todo.id)}
+                style={{ cursor: "pointer", marginLeft: "10px", color: "red" }}
+              >
+                Sil
+              </span>
+            </div>
+          )}
         </div>
       ))}
     </div>
