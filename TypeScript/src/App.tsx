a32449a806fe1bf0a18402fc88feb1c005aca0d8
grbzemir <eyetkingurbuz@trakya.edu.tr>
2024-08-26 14:09:43 +0300

    Second Commit

--- TypeScript/src/App.tsx
+++ TypeScript/src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const name: string = "John Doe";
+  const age: number = 30;
+  const done: boolean = true;
+  const trial: number | string = 5;
+
+  interface Obj {
+    name: string;
+    age: number;
+    done?: boolean;
+  }
+
+  interface ObjTwo extends Obj {
+    // name: string;
+    // age: number;
+    // done?: boolean;
+    accept?: boolean;
+  }
+
+  const obj: ObjTwo = {
+    name: "John Doe",
+    age: 30,
+    done: true,
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      {name} - {age} - {done ? "true" : "false"} - {trial}
+      <br />
+      {obj.name} - {obj.age} - {obj.done ? "true" : "false"} -{trial}
+    </div>
+  );
 }
 
-export default App
+export default App;
